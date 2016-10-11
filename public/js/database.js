1b3b24a73e34fb953535411d44485f23d02a4fc8
Jason Chu <jasonchujc@gmail.com>
2016-10-11 23:43:07 +0800

    Update gameVersion

--- public/js/database.js
+++ public/js/database.js
@@ -19,7 +19,7 @@
 
 // Database management: Performs back-end AJAX queries for data
 
-var gameVersion = 6162
+var gameVersion = 6201
 var championsURL = "//ddragon.leagueoflegends.com/cdn/6.20.1/data/en_US/champion.json"
 var imageURL = "//ddragon.leagueoflegends.com/cdn/6.20.1/img/champion/"
 
