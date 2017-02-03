0885aa8f12bc0b217c0e7d6f2336e09678acfb6b
1lann <me@chuie.io>
2017-02-04 01:46:46 +0800

    Add seperate ranked flex category

--- app/models/crunch/crunch.go
+++ app/models/crunch/crunch.go
@@ -104,10 +104,10 @@ func chomp(player dataFormat.Player, game dataFormat.Game) {
 	normalSR := (game.Type == "NORMAL" ||
 		game.Type == "TEAM_BUILDER_DRAFT_UNRANKED_5x5" ||
 		game.Type == "NORMAL_5x5_DRAFT")
-	rankedSR := (game.Type == "RANKED_SOLO_5x5") ||
-		(game.Type == "RANKED_PREMADE_5x5" || game.Type == "RANKED_FLEX_SR" ||
-			game.Type == "TEAM_BUILDER_DRAFT_RANKED_5x5" ||
-			game.Type == "TEAM_BUILDER_DRAFT_UNRANKED_5x5")
+	rankedSR := (game.Type == "RANKED_SOLO_5x5" ||
+		game.Type == "RANKED_PREMADE_5x5")
+	flexSR := (game.Type == "RANKED_FLEX_SR" ||
+		game.Type == "TEAM_BUILDER_DRAFT_RANKED_5x5")
 	teamBuilder := (game.Type == "CAP_5x5")
 	teamSR := (game.Type == "RANKED_TEAM_5x5")
 	normalTT := (game.Type == "NORMAL_3x3")
@@ -116,7 +116,7 @@ func chomp(player dataFormat.Player, game dataFormat.Game) {
 	aram := (game.Type == "ARAM_UNRANKED_5x5")
 	dynamicQueue := (game.Type == "TEAM_BUILDER_DRAFT_RANKED_5x5")
 	if !(normalSR || rankedSR || teamSR || normalTT || rankedTT || teamTT ||
-		teamBuilder || aram) {
+		teamBuilder || aram || flexSR) {
 		return
 	}
 
@@ -126,6 +126,8 @@ func chomp(player dataFormat.Player, game dataFormat.Game) {
 		parsedType = "Summoner's Rift normals"
 	} else if rankedSR {
 		parsedType = "Summoner's Rift ranked"
+	} else if flexSR {
+		parsedType = "Summoner's Rift ranked flex"
 	} else if dynamicQueue {
 		parsedType = "Summoner's Rift dynamic queue"
 	} else if teamSR {
